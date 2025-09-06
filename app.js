b4b6ed445ace5a41cb4845835b0d895e51c44a29
Altaf Shaik <altaf.shaik@planonsoftware.com>
2025-09-06 22:42:44 +0530

    created separate routes to redirect

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express();
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const db = require('./config/mongoose-connection');
+const ownersRouter = require('./routes/ownersRouter');
+const usersRouter = require('./routes/usersRouter');
+const productsRouter = require('./routes/productsRouter');
 
 app.set('view engine', 'ejs');
 app.use(express.json());
@@ -10,8 +13,8 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use('/owner', ownersRouter);
+app.use('/users', usersRouter);
+app.use('/products', productsRouter);
 
 app.listen(3000);
