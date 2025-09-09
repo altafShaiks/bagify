99a3d57fb291b8706729a9924f61fdbc93daa401
Altaf Shaik <altaf.shaik@planonsoftware.com>
2025-09-09 07:03:34 +0530

    created multer config file and worked with adding products to database

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const isLoggedin = require('../middlewares/isLoggedin');
+const productModel = require('../models/product-model');
 
 router.get('/', (req, res) => {
     let error = req.flash('error');
     res.render('index', {error});
 });
 
-router.get('/shop', isLoggedin, (req, res) => {
-    res.render('shop');
+router.get('/shop', isLoggedin, async (req, res) => {
+    let products = await productModel.find();
+    res.render('shop', {products});
 });
 
 module.exports = router;
--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const upload = require('../config/multer-config');
+const productModel = require('../models/product-model');
 
 router.get('/', function(req, res) {
     res.send('Product');
+});
+
+router.post('/create', upload.single('image'), async function(req, res) {
+    try {
+        let { name, price, discount, bgcolor, panelcolor, textcolor } = req.body;
+        let image = req.file.buffer;
+        let product = await productModel.create({
+            image,
+            name,
+            price,
+            discount,
+            bgcolor,
+            panelcolor,
+            textcolor
+        });
+        req.flash('success', 'Product created successfully!');
+        res.redirect('/owner/admin');
+    } catch (err) {
+        res.send(err.message);
+    }
 });
 
 module.exports = router;
